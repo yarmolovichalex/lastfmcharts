92b4ba040c3303c2a6a2cd38580e8841fbc619fd
yarmolovichalex <yarmolovichalex@gmail.com>
2015-12-22 00:24:43 +0300

    Test10

--- LastFMService/Program.fs
+++ LastFMService/Program.fs
@@ -1,28 +1,28 @@
-﻿//module LastFM
-//
-//open System
-//open FSharp.Configuration
-//open FSharp.Data
-//open FSharp.Data.JsonExtensions
-//
-//type Settings = AppSettings<"App.config">
-//
+﻿module LastFM
+
+open System
+open FSharp.Configuration
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+type Settings = AppSettings<"App.config">
+
 //type Track = { Name: string; Number: int; Length: int }
 //
 //type Album = { Name: string; Tracks: seq<Track> }
-//
-//type Artist = { Name: string; Listeners: int; Plays: int; Similar: seq<string> }
-//
-//let internal processResponse response = 
-//    match response.StatusCode with
-//        | 200 ->
-//            match response.Body with
-//            | Text text -> text
-//            | Binary b -> failwith "Error: not a text"
-//        | _ ->
-//            match response.Body with
-//            | Text text -> failwith text
-//            | Binary b -> failwith "Error: not a text"
+
+type Artist = { Name: string; Listeners: int; Plays: int; Similar: seq<string> }
+
+let internal processResponse response = 
+    match response.StatusCode with
+        | 200 ->
+            match response.Body with
+            | Text text -> text
+            | Binary b -> failwith "Error: not a text"
+        | _ ->
+            match response.Body with
+            | Text text -> failwith text
+            | Binary b -> failwith "Error: not a text"
 //
 //let internal getAlbumInfo artist album =
 //    
@@ -59,53 +59,53 @@
 //        Tracks = tracksSeq
 //    }
 //
-//let internal getArtistInfo artist =
-//
-//    let response = Http.Request(
-//                           Settings.LastfmApi.AbsoluteUri, 
-//                           query = [ "format", "json";
-//                                     "api_key", Settings.LastfmKey;
-//                                     "method", "artist.getInfo";
-//                                     "artist", artist;
-//                                     "autocorrect", "1" ],
-//                           silentHttpErrors = true)
-//
-//    processResponse response
-//
-//let internal parseGetArtistInfoResponse response =
-//    
-//    let response = JsonValue.Parse(response)
-//    
-//    let artist = response.TryGetProperty("artist")
-//    match artist with
-//    | Some(artist)  ->  
-//
-//        let name = artist?name.AsString()
-//        let listeners = artist?stats?listeners.AsInteger()
-//        let plays = artist?stats?playcount.AsInteger()
-//
-//        let similarArray = artist?similar?artist.AsArray()
-//        let similarSeq = seq {
-//            for artist in similarArray do
-//                yield artist?name.AsString()
-//        }
-//
-//        {
-//            Name = name;
-//            Listeners = listeners;
-//            Plays = plays;
-//            Similar = similarSeq
-//        }
-//    | _ -> failwith (response?message.AsString())
+let internal getArtistInfo artist =
+
+    let response = Http.Request(
+                           Settings.LastfmApi.AbsoluteUri, 
+                           query = [ "format", "json";
+                                     "api_key", Settings.LastfmKey;
+                                     "method", "artist.getInfo";
+                                     "artist", artist;
+                                     "autocorrect", "1" ],
+                           silentHttpErrors = true)
+
+    processResponse response
+
+let internal parseGetArtistInfoResponse response =
+    
+    let response = JsonValue.Parse(response)
+    
+    let artist = response.TryGetProperty("artist")
+    match artist with
+    | Some(artist)  ->  
+
+        let name = artist?name.AsString()
+        let listeners = artist?stats?listeners.AsInteger()
+        let plays = artist?stats?playcount.AsInteger()
+
+        let similarArray = artist?similar?artist.AsArray()
+        let similarSeq = seq {
+            for artist in similarArray do
+                yield artist?name.AsString()
+        }
+
+        {
+            Name = name;
+            Listeners = listeners;
+            Plays = plays;
+            Similar = similarSeq
+        }
+    | _ -> failwith (response?message.AsString())
 //
 //let getAlbum artist album =
 //    getAlbumInfo artist album |> parseGetAlbumInfoResponse
 //
-//let getArtist artist =
-//    try
-//        getArtistInfo artist |> parseGetArtistInfoResponse
-//    with 
-//    | ex -> raise(Exception("Failed to get artist '" + artist + "': " + ex.Message))
+let getArtist artist =
+    try
+        getArtistInfo artist |> parseGetArtistInfoResponse
+    with 
+    | ex -> raise(Exception("Failed to get artist '" + artist + "': " + ex.Message))
 //
 //let raiseEx message : unit =
 //    raise (Exception(message))
@@ -113,19 +113,19 @@
 //let raiseEx2 message : unit =
 //    raise (Exception(message))
 
-module LastFM
-
-open System
-open FSharp.Configuration
-open FSharp.Data
-open FSharp.Data.JsonExtensions
-
-type Artist = { Name: string; Listeners: int; Plays: int; Similar: seq<string> }
-
-let getArtist artist =
-    {
-        Name = "Metallica";
-        Listeners = 100;
-        Plays = 200;
-        Similar = [ "Megadeth"; "Anthrax" ]
-    }
+//module LastFM
+//
+//open System
+//open FSharp.Configuration
+//open FSharp.Data
+//open FSharp.Data.JsonExtensions
+//
+//type Artist = { Name: string; Listeners: int; Plays: int; Similar: seq<string> }
+//
+//let getArtist artist =
+//    {
+//        Name = "Metallica";
+//        Listeners = 100;
+//        Plays = 200;
+//        Similar = [ "Megadeth"; "Anthrax" ]
+//    }
