1f55663b2940948e851f7bf003958636dfde7d7c
yarmolovichalex <yarmolovichalex@gmail.com>
2015-12-28 21:49:38 +0300

    commas in scale labels

--- LastFMCharts/Scripts/Compare/chart.js
+++ LastFMCharts/Scripts/Compare/chart.js
@@ -5,6 +5,18 @@ var playsChartContext = new Chart(document.getElementById('plays').getContext('2
 var listenersChart = null;
 var playsChart = null;
 
+var addCommas = function(nStr) {
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
 var fillCharts = function (artistsInfo) {
     if (listenersChart != null) {
         listenersChart.destroy();
@@ -53,7 +65,14 @@ var fillCharts = function (artistsInfo) {
         ]
     };
 
-    listenersChart = listenersChartContext.Bar(listenersData);
-    playsChart = playsChartContext.Bar(playsData);
+    listenersChart = listenersChartContext.Bar(listenersData, {
+        scaleLabel: "<%=addCommas(value)%>",
+        tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= addCommas(value) %>"
+    });
+    playsChart = playsChartContext.Bar(playsData, {
+        scaleLabel: "<%=addCommas(value)%>",
+        tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= addCommas(value) %>"
+    });
+
     window.dispatchEvent(new Event('resize'));
 };
