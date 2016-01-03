5882ad511add3d5138be3d933a3bddb1775e57dd
yarmolovichalex <yarmolovichalex@gmail.com>
2016-01-03 17:35:05 +0300

    refactor

--- LastFMCharts/Controllers/ListenController.cs
+++ LastFMCharts/Controllers/ListenController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using System.Web.Management;
-using System.Web.Mvc;
-using LastFMCharts.Models;
+﻿using System.Web.Mvc;
 
 namespace LastFMCharts.Controllers
 {
@@ -15,68 +9,5 @@ namespace LastFMCharts.Controllers
         {
             return View();
         }
-
-        [HttpPost]
-        public JsonResult Index(string artist, string token)
-        {
-            try
-            {
-                var topTracksNames = LastFM.getTopTracksNames(artist);
-                var tracks = topTracksNames.Select(trackName =>
-                {
-                    var fullTrackName = $"{artist} - {trackName}";
-
-                    var url = VK.getTrackUrl(fullTrackName, token);
-                    return new TrackViewModel
-                    {
-                        Name = trackName,
-                        Url = url
-                    };
-
-                }).ToList();
-
-                return Json(tracks);
-            }
-            catch (Exception ex)
-            {
-                Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                return Json(ex.Message);
-            }
-        }
-
-        //[HttpPost]
-        //public JsonResult Index(string artist, string token)
-        //{
-        //    try
-        //    {
-        //        var topTracksNames = LastFM.getTopTracksNames(artist);
-        //        var tracks = topTracksNames.Select(trackName =>
-        //        {
-        //            var fullTrackName = $"{artist} - {trackName}";
-
-        //            // ignore failed requests (temporary solution)
-        //            try
-        //            {
-        //                var url = VK.getTrackUrl(fullTrackName, token);
-        //                return new TrackViewModel
-        //                {
-        //                    Name = trackName,
-        //                    Url = url
-        //                };
-        //            }
-        //            catch
-        //            {
-        //                return null;
-        //            }
-        //        }).ToList().Where(track => track != null);
-
-        //        return Json(tracks);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Response.StatusCode = (int)HttpStatusCode.BadRequest;
-        //        return Json(ex.Message);
-        //    }
-        //}
     }
 }
